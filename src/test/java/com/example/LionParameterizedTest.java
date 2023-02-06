659b442fe3eff64e59db6eee88f2d9e7e2c07f47
Elena Peremetova <Peremetova.elena@yandex.ru>
2023-02-06 21:28:19 +0300

    Параметризированый тест вынесен в отдельный класс

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+@RunWith(Parameterized.class)
+public class LionParameterizedTest extends TestCase {
+
+    private String sex;
+    private boolean hasMane;
+
+    @Mock
+    Feline feline;
+
+    public LionParameterizedTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters(name = "Пол льва {0}")
+    public static Object[][] getCredentials() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDoesHaveMane() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(hasMane, lion.doesHaveMane());
+    }
+}
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,58 +1,30 @@
 package com.example;
 
 import junit.framework.TestCase;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest extends TestCase {
-
-    private String sex;
-    private boolean hasMane;
+    private static final String SEX = "Самец";
 
     @Mock
     Feline feline;
 
-    public LionTest(String sex, boolean hasMane) {
-        this.sex = sex;
-        this.hasMane = hasMane;
-    }
-
-    @Parameterized.Parameters(name = "Пол льва {0}")
-    public static Object[][] getCredentials() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false},
-        };
-    }
-
-    @Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
     public void testGetKittens() throws Exception {
-        Lion lion = new Lion(sex, feline);
+        Lion lion = new Lion(SEX, feline);
         lion.getKittens();
         Mockito.verify(feline, Mockito.times(1)).getKittens();
     }
 
     @Test
-    public void testDoesHaveMane() throws Exception {
-        Lion lion = new Lion(sex, feline);
-        assertEquals(hasMane, lion.doesHaveMane());
-    }
-
-    @Test
     public void testGetFood() throws Exception {
-        Lion lion = new Lion(sex, feline);
+        Lion lion = new Lion(SEX, feline);
         lion.getFood();
         Mockito.verify(feline, Mockito.times(1)).getFood(Mockito.anyString());
     }
